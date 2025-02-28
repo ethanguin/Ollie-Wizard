2e0ad912437c1e8393e659fecc439d3a142cd8df
ethanguin <ethanielbishop@gmail.com>
2025-02-27 23:07:28 -0700

    edit(camera) polished camera movement with arrow keys and right joystick

--- Characters/PlayerCharacter.gd
+++ Characters/PlayerCharacter.gd
@@ -1,41 +1,30 @@
-# old code with sin
-#extends CharacterBody3D
-#const CAMSTOP_X = 500.0
-#const CAMSPEED = 5
-#const CAMSTOP_Y = CAMSTOP_X
-#
-#@export_group("Camera")
-#@onready var _cam_transform: Node3D = %CamOrigin
-#func _process(delta: float) -> void:
-	#var horizInput = Input.get_axis("cam_left", "cam_right")
-	#var vertInput = Input.get_axis("cam_up", "cam_down")
-	#horizInput = remap(horizInput, -1, 1, -CAMSTOP_X, CAMSTOP_X)
-	#vertInput = remap(vertInput, -1, 1, -CAMSTOP_X, CAMSTOP_X)
-	#var horizPos = horizInput*delta
-	#var vertPos = vertInput*delta
-	#
-	##parabola to slow cam translation. need to rework so it remaps negatives too
-	##_cam_transform.position.x = -(1/SCREENSTOP_X)*(.5*horizPos - SCREENSTOP_X)**2 + SCREENSTOP_X
-	##sin function to ease the translation on edges
-	##_cam_transform.position.x = CAMSTOP_X*sin(horizPos/(2*CAMSTOP_X))
-	##_cam_transform.position.z = CAMSTOP_Y*sin(vertPos/(2*CAMSTOP_Y))
-	#_cam_transform.position.x = horizPos
-	#_cam_transform.position.z = vertPos
-
-# new code with vector math, getting input as a vector. Makes deadspace a circle instead of square-ish
 extends CharacterBody3D
-const CAMSTOP = 500
+
+const ELLIPSEWIDTH = 2.5 # Amount to stretch the x-axis so we can move more horizontally
+const CAMSTOP = 10.0  # Max movement range
+const SPEED = 19.0  # Speed of camera movement
+const RETURN_SPEED = 7.0  # Speed the camera returns to center
 
 @export_group("Camera")
 @onready var _cam_transform: Node3D = %CamOrigin
-func _process(delta: float) -> void:
-	var dirVector = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	
-	
-	#parabola to slow cam translation. need to rework so it remaps negatives too
-	#_cam_transform.position.x = -(1/SCREENSTOP_X)*(.5*horizPos - SCREENSTOP_X)**2 + SCREENSTOP_X
-	#sin function to ease the translation on edges
-	#_cam_transform.position.x = CAMSTOP_X*sin(horizPos/(2*CAMSTOP_X))
-	#_cam_transform.position.z = CAMSTOP_Y*sin(vertPos/(2*CAMSTOP_Y))
-	_cam_transform.position.x = dirVector[0]
-	_cam_transform.position.z = dirVector[1]
+
+var vel := Vector2.ZERO  # Movement speed
+
+func _physics_process(delta: float) -> void:
+	var dirVector = Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
+
+	if dirVector.length() > 0.01:
+		# Move with joystick input
+		vel = dirVector * SPEED
+	else:
+		# Snap back to center when no input
+		var center_offset = Vector2(_cam_transform.position.x, _cam_transform.position.z)
+		vel = -center_offset.normalized() * RETURN_SPEED * center_offset.length()
+
+	# Apply movement
+	var new_x = _cam_transform.position.x + vel.x * delta
+	var new_z = _cam_transform.position.z + vel.y * delta
+
+	# Soft boundary using sin-based easing
+	_cam_transform.position.x = CAMSTOP * sin(new_x / (CAMSTOP * ELLIPSEWIDTH)) * ELLIPSEWIDTH
+	_cam_transform.position.z = CAMSTOP * sin(new_z / CAMSTOP)
