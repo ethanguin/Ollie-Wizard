b705986d0560b466cb22c8913ff92ad7f31dda26
Ethanguin <ethanielbishop@gmail.com>
2025-03-26 12:18:38 -0600

    add(env) forest/ground

--- Characters/PlayerMovement.gd
+++ Characters/PlayerMovement.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
+	var input_dir := Input.get_vector("move_right", "move_left", "move_back", "move_forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
